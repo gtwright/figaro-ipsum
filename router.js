720fdbce85846aac872a0b9cf2ec2859df049b78
gtwright <gtwright@gmail.com>
2016-07-28 20:23:52 -0400

    change q query to p

--- router.js
+++ router.js
@@ -7,7 +7,7 @@ var basicHeader = {'Content-type':'text/html'};
 function home(req, res) {
 	var url_parts = url.parse(req.url, true);
 	var query = url_parts.query;
-	var paragraphs = parseInt(query['q']);
+	var paragraphs = parseInt(query['p']);
 
 	var ipsum = Ipsum(paragraphs);
 	res.writeHeader(200, basicHeader);
