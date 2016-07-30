a867a8782247e3a17807e3546847609380360455
gtwright <gtwright@gmail.com>
2016-07-30 15:31:49 -0400

    Works with select button

---
+++ src/server.js
@@ -0,0 +1,18 @@
+'user strict';
+
+var express = require('express');
+var ipsum = require('./ipsum.js');
+var app = express();
+
+app.set('view engine', 'pug');
+app.set('views', __dirname + '/views');
+app.use('/static', express.static( __dirname + '/public'));
+debugger;
+app.get('/', function(req, res){
+	var paragraph = req.query.p || 2
+	res.render('index', {message: ipsum(paragraph),
+		selected: paragraph
+	});
+});
+
+app.listen(3000);
